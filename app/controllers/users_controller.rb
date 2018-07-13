e72d9893befffa8eaaf3aa7a75720eb68d3795a3
Junki Yamada <fogg.smiths@gmail.com>
2018-07-13 18:23:53 +0900

    Wicked_pdf practice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
+  def index
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf:  "pdffile",
+               layout: 'layouts/pdf.html.erb',
+               show_as_html: params[:debug].present?
+      end
+    end
+  end
 
+  def show
+    @user = User.find(params[:id])
+  end
 end
