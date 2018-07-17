1f30ab8a3f26272fc1aac4bc4cdd8074648e1378
Ryoichi Kira <r.kira@grooves.co.jp>
2018-07-17 18:16:25 +0900

    implement basic confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => ENV['BASIC_AUTH_USERNAME'], :password => ENV['BASIC_AUTH_PASSWORD'] if Rails.env == "staging"
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
